e20070916989a0b6d9f0fb60249aa06c292f8003
Jon Murray <jcokemurray@gmail.com>
2015-01-01 17:39:50 -0700

    Initial commit

---
+++ Subscale/CalcCBParams.m
@@ -0,0 +1,127 @@
+function [TotalWeight,StructureWeight,CGH,MCH,GunwaleHeight,WaterLineHeight,FW,LegMargin,FA,WA,SeatHeight]=CalcCBParams(Lbay,GunPitch,CabinWidth,GunwaleMargin,PersonWeight,SeatDepth,Nbay,PersonScale)
+
+% All lengths in and out in inches. All angles in deg
+
+% ft
+Lbay=Lbay/12;
+CabinWidth=CabinWidth/12;
+GunwaleMargin=GunwaleMargin/12;
+SeatDepth=SeatDepth/12;
+
+% Size structure
+CBeam=.1; % Beam height scale factor (on Lbay)
+BeamHeight=CBeam*Lbay; %ft
+BeamWidth=2/3*BeamHeight; %ft
+CBDens=.5; %lb/ft^2 (sheet density)
+BeamWeight=CBDens*2*(BeamHeight*Lbay+BeamWidth*Lbay);
+SkinWeight=CBDens*(CabinWidth*Lbay+CabinWidth/3*Lbay*2); % approx... assume cabin height = 1/3 width and full floor width as a guess...
+FSeat=.01; % seat structure weight as fraction of person weight (efficiency)
+SeatWeight=FSeat*PersonWeight;
+SWpB=BeamWeight+SkinWeight+SeatWeight; % structure weight per bay (lbs)
+TWpB=PersonWeight+SWpB; % total weight per bay
+
+% Calc bouyancy depth requirement
+rhoWater=1000; %kg/m^3
+TM=TWpB*4.448/9.81; %kg
+CabinWidthWL=CabinWidth-2*(GunwaleMargin/tan(GunPitch/180*pi)); % Cabin width at waterline
+WaterLineHeight=CabinWidthWL*.3048/2*tan(GunPitch/180*pi)-1/2*sqrt((CabinWidthWL*.3048*tan(GunPitch/180*pi))^2-4*TM*tan(GunPitch/180*pi)/(rhoWater*Lbay*.3048));
+if imag(WaterLineHeight)==0
+    WaterLineHeight=WaterLineHeight/.3048; %ft
+    GunwaleHeight=WaterLineHeight+GunwaleMargin;
+    GL=GunwaleHeight/sin(GunPitch/180*pi); % Length of Gunwale piece
+    GW=GL*cos(GunPitch/180*pi);
+    if 2*GW>CabinWidth
+        warning('Gunwale width is larger than specified overall cabin width.')
+        % For calculation purposes, set to NaN
+        FW=NaN;
+        WaterLineHeight=NaN;
+        GunwaleHeight=NaN;
+    else
+        FW=CabinWidth-2*GW; % width of floor
+    end
+else
+    % For calculation purposes, set to NaN
+    FW=NaN;
+    WaterLineHeight=NaN;
+    GunwaleHeight=NaN;
+end
+
+% Calc seat height
+% Calc legroom margin as actual legroom - comfortable legroom (straight
+% upper leg). Assuming 2 ft. lower leg and 1.5 ft upper leg.
+SeatHeight=WaterLineHeight-SeatDepth;
+LLL=2*PersonScale;
+ULL=1.5*PersonScale;
+if SeatHeight<LLL
+    LegMargin=Lbay-(ULL+sqrt(LLL^2-SeatHeight^2));
+else
+    LegMargin=Lbay-ULL;
+end
+
+% Calc CG height
+% Assume person cg is 6 in. above seat, and structure cg is 1/2 of gunwale
+% height...
+CGHS=1/3;
+StructureCG=CGHS*GunwaleHeight; %ft, above floor
+PersonCG=.5*PersonScale;
+CGH=(PersonWeight*(SeatHeight+PersonCG)+SWpB*StructureCG)/TWpB; % ft
+
+% % Test plot bouyancy roll moment about centerline for different roll angles
+% BoatRoll=-10:10; % deg
+% theta1=(GunPitch+BoatRoll)/180*pi;
+% theta2=(GunPitch-BoatRoll)/180*pi;
+% theta3=(BoatRoll)/180*pi;
+% FWm=FW*.3048; %m
+% CenterDepth=WaterLineHeight*.3048; %m
+% d1=CenterDepth-FWm/2*sin(theta3); %m
+% d2=CenterDepth+FWm/2*sin(theta3); %m
+% % bouyancy moment and force (note: force at zero boat roll should equal person
+% % weight)
+% Mb=(Lbay*.3048*rhoWater*9.81*(cos(theta3)./tan(theta1).*d1.^2*FWm/4+d1.^3./(6*tan(theta1).^2)-cos(theta3)./tan(theta2).*d2.^2*FWm/4-d2.^3./(6*tan(theta2).^2)-cos(theta3).^2.*sin(theta3)*FWm^3/12))/(4.448*.3048);  %lb-ft
+% Fb=(Lbay*.3048*rhoWater*9.81*(d1.^2./(2*tan(theta1))+d2.^2./(2*tan(theta2))+CenterDepth*FWm*cos(theta3)))/4.448; %lbs
+% plot(BoatRoll,Mb,BoatRoll,Fb)
+% % Calc metacenter (moment center) height above moment ref point at zero
+% % roll angle. Moment ref point is set at center of floor panel, and boat
+% % roll angle is applied around this point for the purpose of approximating
+% % moment center...
+% % (MetacenterHeight = dMb/dTheta / Fb , where Mb is referenced to a point 
+% % on the y station of the current bouyancy center, i.e. the current zero moment y station)
+% % Need CG height above moment ref to be less than metacenter height above
+% % moment ref for roll stability.
+% MCH=-((Mb(12)-Mb(10))/(2/180*pi))/Fb(11);
+
+
+% Calc metacenter (moment center) height above moment ref point at zero
+% roll angle. Moment ref point is set at center of floor panel, and boat
+% roll angle is applied around this point for the purpose of approximating
+% moment center...
+% (MetacenterHeight = dMb/dTheta / Fb , where Mb is referenced to a point 
+% on the y station of the current bouyancy center, i.e. the current zero moment y station)
+% Need CG height above moment ref to be less than metacenter height above
+% moment ref for roll stability.
+FWm=FW*.3048; %m
+CenterDepth=WaterLineHeight*.3048; %m
+Fb0=(Lbay*.3048*rhoWater*9.81)*(CenterDepth^2/tan(GunPitch/180*pi)+CenterDepth*FWm)/4.448; %lbs
+dMbdtheta0=-(Lbay*.3048*rhoWater*9.81)*(CenterDepth*FWm^2/(2*tan(GunPitch/180*pi))+CenterDepth^2*FWm/2*((1+cos(GunPitch/180*pi)^2)/sin(GunPitch/180*pi)^2)+(2/3)*CenterDepth^3/(tan(GunPitch/180*pi)*sin(GunPitch/180*pi)^2)+FWm^3/12)/(4.448*.3048); %lb-ft
+MCH=-dMbdtheta0/Fb0;
+
+% Frontal area underwater (less frontal area, less potential for pressure
+% drag...)
+FA=(CenterDepth^2/tan(GunPitch/180*pi)+CenterDepth*FWm)/(.3048^2);  % ft^2
+% Wetted area underwater  (less wetted area, less friction drag)
+WA=(FWm+2*CenterDepth/sin(GunPitch/180*pi))/(.3048)*Lbay*Nbay; % ft^2
+
+% Total weight (lbs)
+StructureWeight=SWpB*Nbay;
+TotalWeight=TWpB*Nbay;
+
+% Inches
+GunwaleHeight=GunwaleHeight*12;
+WaterLineHeight=WaterLineHeight*12;
+FW=FW*12;
+LegMargin=LegMargin*12; 
+SeatHeight=SeatHeight*12;
+MCH=MCH*12; 
+CGH=CGH*12;
+
+
---
+++ Subscale/CardBoatSizing.m
@@ -0,0 +1,56 @@
+% Sizing considerations
+
+Lbay=6:1:24; % in
+GunPitch=40:1:75; % deg
+
+
+[L,G]=meshgrid(Lbay,GunPitch);
+
+CabinWidth=12; %in, generally needs to be fixed for decent ability to row with a kayak paddle, but generally less width => less drag but less stability...
+SeatDepth=.3636; % in, below waterline
+GunwaleMargin=1.82; %in, gunwale height margin above waterline...
+PersonWeight=8.4; %lbs
+PersonScale=.3636; % length scale for legroom and person CG metrics...
+Nbay=1; % num of bays (rowers)
+
+TotalWeight=NaN*ones(size(L));
+StructureWeight=NaN*ones(size(L));
+CGH=NaN*ones(size(L));
+MCH=NaN*ones(size(L));
+GunwaleHeight=NaN*ones(size(L));
+WaterLineHeight=NaN*ones(size(L));
+FloorWidth=NaN*ones(size(L));
+LegMargin=NaN*ones(size(L));
+FA=NaN*ones(size(L));
+WA=NaN*ones(size(L));
+SeatHeight=NaN*ones(size(L));
+for i=1:numel(L)  
+    [TotalWeight(i),StructureWeight(i),CGH(i),MCH(i),GunwaleHeight(i),WaterLineHeight(i),FloorWidth(i),LegMargin(i),FA(i),WA(i),SeatHeight(i)]=CalcCBParams(L(i),G(i),CabinWidth,GunwaleMargin,PersonWeight,SeatDepth,Nbay,PersonScale);
+end
+% When NaN is returned for a case, the gunwale pitch was too low to produce
+% necessary depth for bouyancy while still keeping the cabin width as
+% specified. 
+
+% shouldnt make seat depth any larger than it needs to be (seat depth = geo
+% depth. cabin height needs to be higher than waterline depth for
+% flotation... Wan't CG below Moment Center for stability
+StaticMargin=(MCH-CGH)./MCH*100; % static stab margin in percent moment center height
+figure(1)
+[cs,h]=contour(L,G,StaticMargin,[10:10:60],'b-'); % contours of 0, 10, 20, 30% static margin plotted
+clabel(cs,h,'LabelSpacing',500)
+hold on
+[cs,h]=contour(L,G,SeatHeight,0:6,'r-'); % contours of seat level above water
+clabel(cs,h,'LabelSpacing',500)
+[cs,h]=contour(L,G,FloorWidth,[3,6,9],'k-'); % contours of floor width
+clabel(cs,h,'LabelSpacing',500)
+[cs,h]=contour(L,G,LegMargin,[-6,0,6],'g-'); % contours of legroom margin
+clabel(cs,h,'LabelSpacing',500)
+contour(L,G,WA+100*FA,10)  % some sort of drag scaling area
+hold off
+grid on
+xlabel('Lbay (ft)')
+ylabel('Gunwale Pitch (deg)')
+title('Blue: Static Margin (%), Red: Seat Height (in), Black: Floor Width (in), Green: Legroom Margin (in), Colormap: Drag Area (ft^2)')
+
+
+
